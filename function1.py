dbb8a1600ed33f6df5a180796f0a6a58e6410e93
hahm0728 <hahm0728@gmail.com>
2024-04-16 10:52:37 +0900

    클래스연습1

--- function1.py
+++ function1.py
@@ -1,21 +1,51 @@
 # function1.py
+#불변형식
+a = 1.2
+print("a id:", id(a) )
+a = 2.3 
+print("a id:", id(a) )
 
-# 1) 함수 정의
+#가변형식
+lst = [1,2,3]
+print("lst id:", id(lst) )
+lst.append(4)
+print("lst id:", id(lst) )
+print(lst)
 
-def setValue(newValue):
-    x=newValue
-    print("지역변수: ", x)
+#가변형식
+def change(x):
+    #지역변수로 깊은 복사
+    x1 = x[:]
+    x1[0] = "H"
+    print("change함수내부:", x1)
 
-# 2) 호출
+#함수 호출
+wordlist = ["J","A","M"]
+change(wordlist)
+print("함수 호출후:", wordlist)
 
-retValue=setValue(5)
-print(retValue)
+#불변형식인데 읽기/쓰기를 하는 경우
+g = 1 
+def testScope(a):
+    #전역변수를 맵핑
+    #global g 
+    g = 2 
+    return g + a 
 
-# tuple return
-def swap(x,y):
-    return y,x
-# call
-print(swap(*(3,4)))
+#함수 호출
+print( testScope(1) ) 
+print("전역변수 g:", g)
+
+#디버깅 
+#교집합 리턴하는 함수 
+def intersect(prelist, postlist):
+    retList = []
+    for x in prelist:
+        if x in postlist and x not in retList:
+            retList.append(x)
+    return retList 
 
 
+#호출
+print( intersect("HAM", "SPAM") )
 
